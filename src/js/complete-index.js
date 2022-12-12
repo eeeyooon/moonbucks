70884ad7b8d25562bcc669fe5760ba0ed4efee33
jiyoon <vywns9978@gmail.com>
2022-12-12 23:54:20 +0900

    section2 메뉴 추가하기까지 구현된 완성코드

--- src/js/complete-index.js
+++ src/js/complete-index.js
@@ -1,12 +1,3 @@
-/*
-- 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-- 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-- 총 메뉴 갯수를 count하여 상단에 보여준다.
-- 메뉴가 추가되고 나면, input(입력창)은 빈 값으로 초기화한다.
-- 사용자 입력값이 빈 값이라면 추가되지 않는다.
-
-*/
-
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
@@ -14,12 +5,15 @@ function App() {
     e.preventDefault();
   });
 
-  //메뉴의 이름 입력받기 + 메뉴 템플릿 만들기
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      const espressMenuName = $("#espresso-menu-name").value;
-      const menuItemTemplate = (espressMenuName) => {
-        return `<li class="menu-list-item d-flex items-center py-2">
+  const addMenuName = () => {
+    if ($("#espresso-menu-name").value === "") {
+      alert("값을 입력해주세요.");
+      return;
+    }
+
+    const espressMenuName = $("#espresso-menu-name").value;
+    const menuItemTemplate = (espressMenuName) => {
+      return `<li class="menu-list-item d-flex items-center py-2">
       <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
       <button
         type="button"
@@ -34,18 +28,28 @@ function App() {
         삭제
       </button>
     </li>`;
-      };
+    };
 
-      //만든 템플릿 채로 list에 삽입.
-      $("#espresso-menu-list").insertAdjacentHTML(
-        "beforeend",
-        menuItemTemplate(espressMenuName)
-      );
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      menuItemTemplate(espressMenuName)
+    );
 
-      //총 메뉴 갯수를 count하기
-      const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-      $(".menu-count").innerText = `총 ${menuCount} 개`;
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount} 개`;
+
+    $("#espresso-menu-name").value = "";
+  };
+
+  $("#espresso-menu-submit-button").addEventListener("click", () => {
+    addMenuName();
+  });
+
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key !== "Enter") {
+      return;
     }
+    addMenuName();
   });
 }
 
