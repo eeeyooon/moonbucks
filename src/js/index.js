9d664abc086fb26c7239be3c20cec116811039a1
jiyoon <vywns9978@gmail.com>
2023-08-31 21:37:56 +0900

    [🎯 step3] 메뉴 수정 처리하는 Api 객체 생성 및 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -23,6 +23,24 @@ const MenuApi = {
       console.error("에러가 발생했습니다.", response);
     }
   },
+
+  // 메뉴 수정하는 API 함수
+  async updateMenu(category, name, menuId) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다.", response);
+    }
+    return response.json();
+  },
 };
 
 function App() {
@@ -53,7 +71,9 @@ function App() {
   const renderMenu = () => {
     const template = this.menu[this.currentCategory]
       .map((menuItem, index) => {
-        return `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
+        return `<li data-menu-id="${
+          menuItem.id
+        }" class="menu-list-item d-flex items-center py-2">
       <span class="${
         menuItem.soldOut ? "sold-out" : ""
       } w-100 pl-2 menu-name">${menuItem.name}</span>
@@ -111,12 +131,14 @@ function App() {
   };
 
   // 메뉴 수정하는 함수
-  const updateMenuName = (e) => {
+  const updateMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요.", $menuName.innerText);
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
-    store.setLocalStorage(this.menu);
+    await MenuApi.updateMenu(this.currentCategory, updatedMenuName, menuId);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     renderMenu();
   };
 
